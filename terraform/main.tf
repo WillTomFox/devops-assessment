37caf0059f2a0e92132a405c1e7c55f33efbd235
WillTomFox <willtomfox@outlook.com>
2023-08-09 19:12:09 +0100

    Trial fix for VPC limit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,26 @@
-resource "aws_vpc" "devops_assessment_vpc" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "devops_assessment_vpc" {
+  id = "vpc-089a68f18c73bf566"
 }
 
 resource "aws_subnet" "devops_assessment_subnet" {
-  vpc_id     = aws_vpc.devops_assessment_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id     = data.aws_vpc.devops_assessment_vpc.id
+  cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_security_group" "devops_assessment_security_group" {
-  name_prefix = "devops-assessment-security-group"
+  name   = "devops-assessment-security-group"
+  vpc_id = data.aws_vpc.devops_assessment_vpc.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_ecs_cluster" "devops_assessment_ecs_cluster" {
+  name     = "devops-assessment-ecs-cluster"
 }
 
 resource "aws_ecs_task_definition" "devops_assessment_task" {
@@ -23,9 +28,9 @@ resource "aws_ecs_task_definition" "devops_assessment_task" {
   network_mode             = "awsvpc"
   container_definitions    = jsonencode([{
     name  = "devops-assessment"
-    image = "devops_assessment:latest"
+    image = "303981612052.dkr.ecr.region.amazonaws.com/devops_assessment:latest"
     portMappings = [{
-      containerPort = 443
+      containerPort = 8080
       protocol      = "tcp"
     }]
   }])
@@ -36,7 +41,7 @@ resource "aws_ecs_task_definition" "devops_assessment_task" {
 
 resource "aws_ecs_service" "devops_assessment_ecs_service" {
   name            = "devops-assessment-ecs-service"
-  cluster         = "default"
+  cluster         = aws_ecs_cluster.devops_assessment_ecs_cluster.arn
   task_definition = aws_ecs_task_definition.devops_assessment_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
