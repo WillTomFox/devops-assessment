9721353fc08705a797c00a14d6ea4974566b1dde
WillTomFox <willtomfox@outlook.com>
2023-08-10 12:46:30 +0100

    Map public IP on launch

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -7,6 +7,14 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "devops_assessment_subnet" {
-  vpc_id     = data.aws_vpc.devops_assessment_vpc.id
+  vpc_id                = data.aws_vpc.devops_assessment_vpc.id
   cidr_block = "10.0.2.0/24"
+  # count                   = "${length(var.public_subnets_cidr)}"
+  # cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
+  # availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = true
+  # tags = {
+  #   Name        = "${var.environment}-${element(var.availability_zones, count.index)}-      public-subnet"
+  #   Environment = "${var.environment}"
+  # }
 }
