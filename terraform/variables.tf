a040e982dd9e077f7eefc1d5d311bea4c43a7b24
WillTomFox <willtomfox@outlook.com>
2023-08-10 12:25:56 +0100

    Fix ports

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,14 @@
+# AWS access key for personal account
 variable "access_key" {
   type = string
 }
 
+# AWS secret key for personal account
 variable "secret_key" {
   type = string
 }
 
+# AWS account ID for TP account
 variable "aws_account_id" {
   type = string
 }
